0ae1f21215d9cffa3f27eed3db1d7553976db46f
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-02-28 01:01:06 -0500

    inicia proyecto PF

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+// import RoutesApp from "./routes/RoutesApp";
+function App() {
+  return (
+    <>
+      {/* <RoutesApp /> */}
+      <h1>Estamos en linea..</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <>
+      <h1>404 ERROR</h1>
+    </>
+  );
+}
+
+export default NotFound;
