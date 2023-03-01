763f69e7c5d5d2895f335eaf21f82cfc81e107a6
javimt <javiergp_19@outlook.com>
2023-03-01 15:58:31 -0500

    parte del componente detail

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,13 @@
-
 //import RoutesApp from "./routes/RoutesApp.jsx";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home, About, Contact, LandingPage, NotFound } from "./pages/index"; 
 
-// import RoutesApp from "./routes/RoutesApp";
-import Cards from "./components/Cards/Cards"
+
 function App() {
   return (
     <>
       {/* <RoutesApp /> */}
-
     <Routes>
       <Route exact path="/" element={<LandingPage />} />
       <Route exact path="home" element={<Home />} />
@@ -18,9 +15,8 @@ function App() {
       <Route exact path="contact" element={<Contact />} />
       <Route exact path="*" element={<NotFound />} />
     </Routes>
-      <Cards/>
     </>
   );
 }
 
-export default App;
+export default App; 
--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,32 +1,39 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
+import style from './About.module.css';
+import data from "../../data.json";
 
 function About() {
-
-  const { id } = useParams();
+console.log(data)
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const [, setDetailId] = useState(null);
 
 
-  return (
-    <div>
-      <div>
+  return data.hasOwnProperty("name") ? (
+    <div className={style.about}>
+      <div className={style.back}>
         <Link to='/home'>
           <button>Go Back</button>
         </Link>
       </div>
-      <h3>año</h3>
-      <h3>color</h3>
-      <h3>modelo</h3>
-      <h3>imagen</h3>
-      <h3>tipo de transmision</h3>
-      <h3>numero de puertas</h3>
-      <h3>categoria</h3>
-      <h3>kilometraje</h3>
-      <h3>tipo de combustible</h3>
-      <h3>descripcion</h3>
+        <div className={style.container} key={data.id}>
+          <h3>brand: {data.brand}</h3>
+          <h3>year: {data.year}</h3>
+          <h3>color: {data.color}</h3>
+          <h3>model: {data.model}</h3>
+          <h3>image: {data.image}</h3>
+          <h3>transmission: {data.transissionType}</h3>
+          <h3>doors: {data.doors}</h3>
+          <h3>category: {e.category}</h3>
+          <h3>mileage: {e.mileage}</h3>
+          <h3>fuelType: {e.fuelType}</h3>
+          <h3>desciption: {e.description}</h3>
+          <br />
+        </div>
     </div>
-  )
+  ) : <h2>Loading...</h2>
 }
 
 export default About
