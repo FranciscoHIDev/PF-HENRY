0c118708b9efe96b5f0b2bb7f0091d0b6ece6931
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-03-01 15:09:13 -0500

    se crea footer

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Cards from "../../components/Cards/Cards";
 import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
 
 function Home() {
   return (
     <>
       <NavBar />
       <Cards />
+      <Footer />
     </>
   );
 }
