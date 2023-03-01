e4eacc32ee34c3e9e5d80775c866a96faf7da6df
javimt <javiergp_19@outlook.com>
2023-02-28 22:37:35 -0500

    parte del componente detail

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
-// import RoutesApp from "./routes/RoutesApp";
+//import RoutesApp from "./routes/RoutesApp.jsx";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home, About, Contact, LandingPage, NotFound } from "./pages/index"; 
+
+
 function App() {
   return (
     <>
       {/* <RoutesApp /> */}
-      <h1>Estamos en linea..</h1>
+    <Routes>
+      <Route exact path="/" element={<LandingPage />} />
+      <Route exact path="home" element={<Home />} />
+      <Route exact path="about" element={<About />} />
+      <Route exact path="contact" element={<Contact />} />
+      <Route exact path="*" element={<NotFound />} />
+    </Routes>
     </>
   );
 }
