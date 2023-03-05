20414a5ca6160763ac3909bbb8bb9bfa4d513365
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-03-05 02:03:24 -0500

    actualizacion card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -17,7 +17,7 @@ function Card({
   _id,
 }) {
   return (
-    <div className="flex flex-col w-[360px] h-[500px] rounded-lg mb-12 mx-4  bg-zinc-100  items-center shadow-md ">
+    <div className="flex flex-col w-[360px] h-[500px] rounded-lg mb-12 mx-4  bg-zinc-100  items-center shadow-md hover:scale-105">
       <img
         className="rounded-t-lg h-[190px] w-[360px] bg-white"
         src={image}
@@ -37,7 +37,7 @@ function Card({
           <MdFavoriteBorder className="text-[#FB8500] cursor-pointer  text-[22px]" />
         </p>
       </div>
-      <div className="flex flex-row mb-1">
+      <div className="flex flex-row mb-1 ">
         <p className="flex mb-2 mr-5 text-black">
           <MdHdrAutoSelect className="text-[#FB8500] text-3xl" />
           <span className="ml-2 ">{transissionType}</span>
