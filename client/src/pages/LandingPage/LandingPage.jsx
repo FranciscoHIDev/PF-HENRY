330128c404edc326375d1f85ce1091ead13d88f7
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-03-05 14:51:31 -0500

    cambios landing, navbar

--- client/src/pages/LandingPage/LandingPage.jsx
+++ client/src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Style from "../LandingPage/LandingPage.module.css";
 import { Link } from "react-router-dom";
-import NavBar from '../../components/NavBar/NavBar'
+import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
 function LandingPage() {
   const [lenguaje, setLenguje] = React.useState({
@@ -9,16 +9,15 @@ function LandingPage() {
     boton: "Entrar!",
   });
   function onButtonClick(e) {
-
-    e.target.value === "EN" ?
-      setLenguje({
-        titulo: "The site You can buy yor Car",
-        boton: "GO!"
-      }) : setLenguje({
-        titulo: "EL sitio donde compras tu auto!",
-        boton: "Entrar!"
-      })
-
+    e.target.value === "EN"
+      ? setLenguje({
+          titulo: "The site You can buy yor Car",
+          boton: "GO!",
+        })
+      : setLenguje({
+          titulo: "EL sitio donde compras tu auto!",
+          boton: "Entrar!",
+        });
 
     e.target.value === "EN"
       ? setLenguje({
@@ -31,30 +30,30 @@ function LandingPage() {
         });
   }
   return (
-    <div className={Style.container}>
-      <div className={Style.botones}>
-        <NavBar onButtonClick={onButtonClick} className="w-100vw"/>
-        {/* <button className=" mr-2" onClick={onButtonClick} value="EN">
+    <>
+      <NavBar />
+      <div className={Style.container}>
+        <div className={Style.botones}>
+          {/* <button className=" mr-2" onClick={onButtonClick} value="EN">
           EN
         </button>
         <button className=" mr-2" onClick={onButtonClick} value="ES">
           ES
         </button> */}
-        </button>
+        </div>
+
+        <div className={Style.titulo}>
+          <h1>{lenguaje.titulo}</h1>
+        </div>
+        <div className={Style.boxBtn}>
+          <Link to="/home">
+            <button>{lenguaje.boton}</button>
+          </Link>
+        </div>
+        <div className={Style.footer}></div>
       </div>
-      <div className={Style.titulo}>
-        <h1>{lenguaje.titulo}</h1>
-      </div>
-      <div className={Style.boxBtn}>
-        <Link to="/home">
-          <button>{lenguaje.boton}</button>
-        </Link>
-      </div>
-    </div>
-      <div className={Style.footer}>
-        <Footer/>
-      </div>      
-    </div>
+      <Footer />
+    </>
   );
 }
 
