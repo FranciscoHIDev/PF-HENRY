8a9c81bd65b2c4ae72c8148e43f6c57a38a7aaa2
Daniel Augusto Quijada Higgins <mago0oxx@gmail.com>
2023-03-07 19:38:25 -0300

    cambio

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,19 +6,16 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store/store";
 import { Provider } from "react-redux";
 // PRUEBA DE AUTH0
-import { Auth0Provider } from '@auth0/auth0-react'
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
 
     <BrowserRouter>
-      <Auth0Provider
-        domain="dev-qnvnr45ttbe81bet.us.auth0.com"
-        clientID="Fp70Q2d5uwT1hOaXTEOK0rJGINGGicYA"
-        rediretUrl={window.location.origin}> 
+     
         <App />
-      </Auth0Provider> 
+      
 
     </BrowserRouter>
   </Provider>
