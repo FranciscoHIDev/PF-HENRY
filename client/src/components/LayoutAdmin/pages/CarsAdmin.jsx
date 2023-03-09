4f6b14665069093a9efa61ec04dc238368d0de9f
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-03-09 13:31:24 -0500

    update2

--- client/src/components/LayoutAdmin/pages/CarsAdmin.jsx
+++ client/src/components/LayoutAdmin/pages/CarsAdmin.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+// import {
+//   FormControlLabel,
+//   MenuItem,
+//   Radio,
+//   RadioGroup,
+//   Table,
+//   TableContainer,
+//   TableHead,
+//   TableRow,
+//   TableCell,
+//   TableBody,
+//   Modal,
+//   Button,
+//   TextField,
+// } from "@mui/material";
 
 function CarsAdmin() {
   return (
     <>
-      <p>All cars</p>
+      <h1>Hola como estas</h1>
     </>
   );
 }
-
 export default CarsAdmin;
