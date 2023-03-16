7e1de4774456daca99f62f00c785b36ce9aad041
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-03-16 14:10:29 -0500

    Crear usuarios funcionando

--- api/src/Models/Users.js
+++ api/src/Models/Users.js
@@ -4,17 +4,16 @@ const userSchema = mongoose.Schema({
     name: {
         type: String,
         required: true,
-        minLength: 3,
-        maxLength: 20,
+
     },
     image: {
         type: String,
-        default: "http://cdn.onlinewebfonts.com/svg/img_141364.png",
+
     },
     lastname: {
         type: String,
-        minLength: 3,
-        maxLength: 20,
+
+
     },
     dni: {
         type: Number,
@@ -61,6 +60,8 @@ const userSchema = mongoose.Schema({
         {
             type: mongoose.Types.ObjectId,
             ref: "Review",
+            minLength: 30,
+            maxLength: 300,
         },
     ],
     // password: {
