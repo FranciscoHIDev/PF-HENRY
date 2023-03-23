3f3525d5d25612d282759d660f9e30a00dfdd59d
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-03-23 03:37:25 -0500

    Update MPButton.jsx

--- client/src/components/MPButton/MPButton.jsx
+++ client/src/components/MPButton/MPButton.jsx
@@ -64,7 +64,7 @@ export function MPButton({ id }) {
   //-----------------------
   const fetchCheckout = async () => {
     const res = await fetch(
-      "https://deploying-pf-production.up.railway.app/cars/comprar",
+      "https://deploying-back-pf-production.up.railway.app/cars/comprar",
       {
         method: "POST",
         headers: {
