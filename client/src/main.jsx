77a9ec8611e8821ee064c1b26e9d0d4ece41d773
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-03-27 09:02:58 -0500

    Ultima actualizacion..

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,16 +8,15 @@ import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001";
-//axios.defaults.baseURL = "https://deploying-back-pf-production.up.railway.app";
-
+//axios.defaults.baseURL = "http://localhost:3001";
+axios.defaults.baseURL = "https://deploying-back-pf-production.up.railway.app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <Auth0Provider
-         domain="dev-vgc7jwfb4gz8q64r.us.auth0.com"
-         clientId="b08kk8UHwGoECWk7kloms5RxdQ7VyuwR"
+        domain="dev-vgc7jwfb4gz8q64r.us.auth0.com"
+        clientId="b08kk8UHwGoECWk7kloms5RxdQ7VyuwR"
         authorizationParams={{
           redirect_uri: window.location.origin,
         }}
