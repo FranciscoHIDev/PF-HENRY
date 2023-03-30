294b025b0e2c44b493d31f0359d1bfa7b8d36955
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-03-29 21:36:17 -0500

    Update NavBar.jsx

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -104,11 +104,7 @@ function NavBar() {
                     transition
                     menuClassName="bg-secondary-100 p-4"
                   >
-                    <MenuItem className="p-0 hover:bg-transparent">
-                      <Link
-                        to="/profile"
-                        className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
-                      >
+                    <MenuItem className="p-0 hover:bg-transparent">                      
                         <img
                           src={user.picture}
                           alt={user.given_name}
@@ -119,8 +115,7 @@ function NavBar() {
                           <span className="text-xs text-gray-500">
                             {user.email}
                           </span>
-                        </div>
-                      </Link>
+                        </div>                      
                     </MenuItem>
                     <hr className="my-4 border-gray-500" />
                     <MenuItem className="p-0 hover:bg-transparent">
@@ -142,12 +137,15 @@ function NavBar() {
                       )}
                     </MenuItem>
                     <MenuItem className="p-0 hover:bg-transparent">
-                      <Link
-                        to="/profile/favorites"
-                        className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
-                      >
-                        <MdOutlineFavorite /> Favorites
-                      </Link>
+                      {infoUser.roll == "superAdmin" ||
+                      infoUser.roll == "Admin" ? null : (
+                        <Link
+                          to="/profile/favorites"
+                          className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
+                        >
+                          <MdOutlineFavorite /> Favorites
+                        </Link>
+                      )}
                     </MenuItem>
                     <MenuItem className="p-0 hover:bg-transparent">
                       <Link
@@ -229,11 +227,7 @@ function NavBar() {
                     transition
                     menuClassName="bg-secondary-100 p-4"
                   >
-                    <MenuItem className="p-0 hover:bg-transparent">
-                      <Link
-                        to="/profile"
-                        className="rounded-lg transition-colors text-gray-300 hover:bg-secondary-900 flex items-center gap-x-4 py-2 px-6 flex-1"
-                      >
+                    <MenuItem className="p-0 hover:bg-transparent">                    
                         <img
                           src={user.picture}
                           alt={user.given_name}
@@ -244,8 +238,7 @@ function NavBar() {
                           <span className="text-xs text-gray-500">
                             {user.email}
                           </span>
-                        </div>
-                      </Link>
+                        </div>                      
                     </MenuItem>
                     <hr className="my-4 border-gray-500" />
                     <MenuItem className="p-0 hover:bg-transparent">
